68f4a1ea6760f0f552e0e17e5cb3eb8279551183
Minori Yamashita <ympbyc@gmail.com>
2013-05-24 09:03:42 +0900

    treat null and undefined as nothing

--- underscore-fix-monad.js
+++ underscore-fix-monad.js
@@ -31,6 +31,12 @@ if (typeof CLOS === "undefined") throw "JS-CLOS is missing";
     CLOS.define_method(bind, [Nothing, "function"], function (n, f) {
         return n;
     });
+    CLOS.define_method(bind, [null, "function"], function (n, f) {
+        return n;
+    });
+    CLOS.define_method(bind, [undefined, "function"], function (n, f) {
+        return n;
+    });
     CLOS.define_method(bind, [Just, "function"], function (j, f) {
         return f(j._monad_val);
     });
